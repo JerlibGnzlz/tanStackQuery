3765cc6bc1b36dba73daba768b9f8828bde95a91
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-02-27 16:01:17 -0300

    feature utilizando fetch

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+
+const getLabels = async () => {
+  const resp = await fetch(
+    "https://api.github.com/repos/facebook/react/labels"
+  );
+  const data = await resp.json();
+  console.log(data);
+  return data;
+};
 
 export const LabelPicker = () => {
+  const labelsQuery = useQuery({ queryKey: ["labels"], queryFn: getLabels });
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}
+      >
+        Primary
+      </span>
     </div>
-  )
-}
+  );
+};
