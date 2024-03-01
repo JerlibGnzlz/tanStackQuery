f4c02f5c42e5fc48b2e9c1eddfb3fcd6f094d82b
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-02-29 22:01:49 -0300

    auth token gitHub

--- src/api/githubApi.tsx
+++ src/api/githubApi.tsx
@@ -2,5 +2,8 @@ import axios from "axios";
 
 export const gitHubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
-  headers: {},
+  headers: {
+    Authorization:
+      "Bearer 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",
+  },
 });
