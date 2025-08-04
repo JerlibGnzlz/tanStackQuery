eba3c86b5db3e7dccb75de097b495b05bdfe444d
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-08-04 00:38:00 -0300

    Remove secret and update files

--- src/api/githubApi.tsx
+++ src/api/githubApi.tsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+
 export const gitHubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
   headers: {
     Authorization:
-      "Bearer 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",
+      `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
   },
-});
+}); 
